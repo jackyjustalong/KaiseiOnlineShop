0f5f40831fd26044288285634e0ca6be236ef023
justalongjapan <justalongjapan@gmail.com>
2023-12-13 17:26:50 +0900

    request

--- src/main/java/tokyo/kaisei/controller/UserController.java
+++ src/main/java/tokyo/kaisei/controller/UserController.java
@@ -1,6 +1,8 @@
 package tokyo.kaisei.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,14 +21,13 @@ public class UserController {
 		return user;
 	}
 	
-	@RequestMapping("/login")
-	public Users userLogin(String username, String password) {
+	@PostMapping("/login")
+	public Users userLogin(@RequestBody Users loginUser) {
+		 // Assuming Users class has fields 'username' and 'password'
+        String username = loginUser.getUsername();
+        String password = loginUser.getPassword();
+		
 		Users user = userService.findUserByUsernamePassword(username,password);
 		return user;
 	}
-	
-	
-	
-
-	
 }
